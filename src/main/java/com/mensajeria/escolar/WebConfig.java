0fecec9f9c5f56b55367f3ec52fea16d62c8fdca
RamonCruz187 <ramon.cruz187@gmail.com>
2024-07-21 11:55:16 -0300

    Update CORS

--- src/main/java/com/mensajeria/escolar/WebConfig.java
+++ src/main/java/com/mensajeria/escolar/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings (CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
 }
 }
